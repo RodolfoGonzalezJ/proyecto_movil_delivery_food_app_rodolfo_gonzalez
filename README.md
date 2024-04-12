c79d52ae9d92e6faaf6ca1e129be560d8adda475
RodolfoGonzalezJ <rgonzalezjimenez8@gmail.com>
2024-04-11 20:21:57 -0500

    Primer Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# proyecto_movil_delivery_food_app_rodolfo_gonzalez
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/proyecto_movil_delivery_food_app_rodolfo_gonzalez/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.proyecto_movil_delivery_food_app_rodolfo_gonzalez
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
