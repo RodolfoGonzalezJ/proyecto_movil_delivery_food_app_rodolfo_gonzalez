8e670b0125ca775d7f53aad711ef66f0a26bc489
RodolfoGonzalezJ <rgonzalezjimenez8@gmail.com>
2024-04-14 15:51:38 -0500

    Firebase and welcome_screen and sign_in_screen and sign_up_screen

--- lib/app_view.dart
+++ lib/app_view.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:proyecto_movil_delivery_food_app_rodolfo_gonzalez/blocs/authentication_bloc/authentication_bloc.dart';
+
+import 'screens/auth/views/welcome_screen.dart';
+import 'screens/home/views/home_screen.dart';
 
 class MyAppView extends StatelessWidget {
   const MyAppView({super.key});
@@ -6,7 +11,22 @@ class MyAppView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Delivery Food App',
-    );
+        title: 'Pizza Delivery App',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+            colorScheme: ColorScheme.light(
+                background: Colors.grey.shade100,
+                onBackground: Colors.black,
+                primary: Colors.blue,
+                onPrimary: Colors.white)),
+        home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
+          builder: ((context, state) {
+            if (state.status == AuthenticationStatus.authenticated) {
+              return const HomeScreen();
+            } else {
+              return const WelcomeScreen();
+            }
+          }),
+        ));
   }
 }
