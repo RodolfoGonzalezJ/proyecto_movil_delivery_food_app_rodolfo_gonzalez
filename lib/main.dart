6a10fda766a2be7bb27fe46a4a045715a0f58632
RodolfoGonzalezJ <rgonzalezjimenez8@gmail.com>
2024-04-13 23:35:07 -0500

    firebase_user_repo and authentication_bloc and bloc_observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:proyecto_movil_delivery_food_app_rodolfo_gonzalez/app.dart';
+import 'package:proyecto_movil_delivery_food_app_rodolfo_gonzalez/simple_bloc_observer.dart';
+import 'package:user_repository/user_repository.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  Bloc.observer = SimpleBlocObserver();
+  runApp(MyApp(FirebaseUserRepo()));
 }
